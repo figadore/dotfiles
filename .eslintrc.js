c8c5a540a47fd06c8f9cf7b2e2a4d251d32f653b
Reese Wilson <Reese.Wilson@healthsparq.com>
2017-12-28 13:47:32 -0800

    add eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  //root: true,
+  //env: {
+  //  browser: false
+  //},
+  //rules: {
+  //  // "no-unused-vars": ["error", { "args": "none" }],
+  //  // examples - how to enable additional rules
+
+  //  //"indent": ["error", 4],
+  //  //"linebreak-style": ["error", "unix"],
+  //  //"quotes": ["error", "double"],
+  //  //"semi": ["error", "always"],
+
+  //  // override default options for rules from base configurations
+  //  //"comma-dangle": ["error", "always"],
+  //  //"no-cond-assign": ["error", "always"],
+
+  //  // disable rules from base configurations
+  //  //"no-console": "off",
+  //},
+  //settings: {
+  //    "import/resolver": "node"
+  //},
+  extends: 'ccs/node'
+}
